f72dfedc1e88eecddd38c48e9d0d96d7053e306e
Mika Säppi <mika.sappi@collinsgroup.fi>
2026-02-04 12:30:03 +0200

    version update

--- go/main.go
+++ go/main.go
@@ -35,7 +35,7 @@ import (
 )
 
 const (
-	currentVersion = "1.3.0"
+	currentVersion = "1.2.3"
 	versionCheckURL = "https://software.collinsgroup.fi/tnt-version.json"
 	//macARMDownloadURL = "https://software.collinsgroup.fi/TNT.dmg"
 	//macIntelDownloadURL = "https://software.collinsgroup.fi/TNT-Intel.dmg"
